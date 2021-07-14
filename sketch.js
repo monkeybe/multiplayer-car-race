150f31cb8dadb4b97d18af9ec064e4128d347478
monkeybe <82957083+monkeybe@users.noreply.github.com>
2021-07-14 20:02:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var  database;
+var canvas, backgroundImage;
+var gameState = 0;
+var playerCount;
+var form, game, player;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+game = new Game();
+game.getState();
+game.start();
+
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
